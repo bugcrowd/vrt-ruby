20477a2a8d45b3f180a545996d4c9b8a1ac593c8
devin <devin@bugcrowd.com>
2018-04-24 10:27:52 -0700

    Add remediation advice mapping

--- lib/vrt/mapping.rb
+++ lib/vrt/mapping.rb
@@ -14,19 +14,25 @@ module VRT
         id_list = VRT.find_node(vrt_id: id_list.join('.'), preferred_version: @min_version).id_list
         version = @min_version
       end
-
-      # iterate through the id components, keeping track of where we are in the mapping file
-      # and the most specific mapped value found so far
       mapping = @mappings[version]['content']
-      best_guess = @mappings[version]['metadata']['default']
-      id_list.each do |id|
-        entry = mapping[id]
-        break unless entry # mapping file doesn't go this deep, return previous value
-        best_guess = entry[@scheme] if entry[@scheme]
-        # use the children mapping for the next iteration
-        mapping = entry['children'] || {}
+      default = @mappings[version]['metadata']['default']
+      keys = @mappings[version]['metadata']['keys']
+      if keys
+        # Convert mappings with multiple keys to live under a single
+        # top-level key. Remediation advice has keys 'remediation_advice'
+        # and 'references' so we convert it to look like
+        # { remediation_advice: { remediation_advice: 'advice', references: [...] } }
+        keys.each_with_object({}) do |key, acc|
+          acc[key.to_sym] = get_key(
+            id_list: id_list,
+            mapping: mapping,
+            key: key,
+            default: default&.try(:[], key)
+          )
+        end
+      else
+        get_key(id_list: id_list, mapping: mapping, key: @scheme, default: default)
       end
-      best_guess
     end
 
     private
@@ -50,13 +56,36 @@ module VRT
     # becomes
     #     {one: {'id': 'one', 'foo': 'bar'}, two: {'id': 'two', 'foo': 'baz'}}
     def key_by_id(mapping)
-      case mapping
-      when Array
+      if mapping.is_a?(Array) && mapping.first.is_a?(Hash) && mapping.first.key?('id')
         mapping.each_with_object({}) { |entry, acc| acc[entry['id'].to_sym] = key_by_id(entry) }
-      when Hash
+      elsif mapping.is_a?(Hash)
         mapping.each_with_object({}) { |(key, value), acc| acc[key] = key_by_id(value) }
       else
         mapping
+      end
+    end
+
+    def get_key(id_list:, mapping:, key:, default:)
+      # iterate through the id components, keeping track of where we are in the mapping file
+      # and the most specific mapped value found so far
+      best_guess = default
+      id_list.each do |id|
+        entry = mapping[id]
+        break unless entry # mapping file doesn't go this deep, return previous value
+        best_guess = merge_collections(best_guess, entry[key]) if entry[key]
+        # use the children mapping for the next iteration
+        mapping = entry['children'] || {}
+      end
+      best_guess
+    end
+
+    def merge_collections(previous_value, new_value)
+      if previous_value.is_a?(Array) && new_value.is_a?(Array)
+        new_value + previous_value
+      elsif previous_value.is_a?(Hash) && new_value.is_a?(Hash)
+        previous_value.merge(new_value)
+      else
+        new_value
       end
     end
   end
