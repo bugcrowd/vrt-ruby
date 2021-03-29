2b60cbce73b415db271176ca2141d19fe6b284e8
Adam David <adamrdavid@gmail.com>
2021-03-29 14:52:38 -0700

    Add example if we want to use it later

--- lib/vrt/configuration.rb
+++ lib/vrt/configuration.rb
@@ -13,3 +13,23 @@ module VRT
     end
   end
 end
+
+# Example usage:
+
+# ```ruby
+# def get_lineage(string, max_depth: 'variant')
+#   @_lineages[string + max_depth] ||= construct_lineage(string, max_depth, VRT.configuration.lineage_string_separator)
+# end
+# # ...
+# private
+# # ...
+# def construct_lineage(string, max_depth, string_separator: nil)
+#   return unless valid_identifier?(string)
+#   lineage = []
+#   walk_node_tree(string, max_depth: max_depth) do |node|
+#     return unless node
+#     lineage << node.name
+#   end
+#   string_separator.nil? ? lineage : lineage.join(string_separator)
+# end
+# ```
